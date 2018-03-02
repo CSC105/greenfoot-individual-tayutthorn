08c9def47b5ad4923121b42ef2293a872eacaac9
tayutthorn <36398329+tayutthorn@users.noreply.github.com>
2018-03-02 13:50:37 +0700

    add sound , scoreboard

---
+++ Gameover.java
@@ -0,0 +1,37 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Gameover here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Gameover extends World
+{
+    //GreenfootSound myMusic = new GreenfootSound("trollsong.mp3");
+    //public int score =0;
+     
+    
+    /**
+     * Constructor for objects of class Gameover.
+     * 
+     */
+    public Gameover()
+    {    
+
+        super(1280, 720, 1); 
+
+    }
+
+    public void act()
+    {
+        Display.myMusic.stop(); 
+        if(Greenfoot.isKeyDown("R"))
+            Greenfoot.setWorld(new Display());
+       
+        showText("Your Score is = "+ Display.score ,1000,600);
+    }
+
+
+    //
+}
